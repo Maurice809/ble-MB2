ab277ba4407eb39bf605833fd382392a5081d259
Maurice <tmoret@student.42lausanne.ch>
2023-11-06 09:59:17 +0000

    42 Lausanne - Modification mineur

--- main.ts
+++ main.ts
@@ -5,27 +5,27 @@ input.onButtonPressed(Button.A, function () {
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "TEST") {
         basic.showIcon(IconNames.Heart)
+        basic.pause(100)
         radio.sendString("CHECK")
     }
     if (receivedString == "CHECK") {
         basic.showIcon(IconNames.Yes)
-    } else {
-    	
     }
 })
 input.onButtonPressed(Button.B, function () {
-    ecran = "TX = " + tx
+    ecran = "TX" + tx
     basic.showString(ecran)
     tx += 1
     if (tx == 8) {
-        tx = 0
+        tx = 1
     }
-    radio.setTransmitPower(tx)
 })
-let tx = 0
 let ecran = ""
+let tx = 0
 radio.setGroup(1)
 basic.showString("BLE")
+tx += 1
+radio.setTransmitPower(tx)
 basic.forever(function () {
 	
 })
